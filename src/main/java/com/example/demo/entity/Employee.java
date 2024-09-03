833048c49a5e5f1d2556d188c1d954530d6bfc7a
Shivam Singh <Shivamsingh2211653@gmail.com>
2024-09-03 23:59:18 +0530

    updating Employee.java file

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,36 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+//import javax.validation.constraints.Email;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.annotations.processing.Pattern;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long empId;
+
+
+//    @NotNull(message = "Name should Not be Null")
+    @NotNull
+    private String name;
+
+//    @Email(message="Please provide a valid email address")
+    private String emailId;
+
+//    @Pattern(regexp="(^$|[0-9]{10})")
+    private String mobNo;
+    private String address;
+}
