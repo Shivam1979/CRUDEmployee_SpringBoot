6b1d341ba777366face145e5ed29ba661a48871b
Shivam Singh <Shivamsingh2211653@gmail.com>
2024-09-04 21:07:12 +0530

    adding service file

--- src/main/java/com/example/demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -1,2 +1,33 @@
-package com.example.demo.service;public class EmployeeServiceImpl {
+package com.example.demo.service;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.exception.EmployeeException;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService{
+    @Override
+    public Employee saveEmployee(Employee employee) throws EmployeeException {
+        return null;
+    }
+
+    @Override
+    public Employee getEmployeeById(Integer id) throws EmployeeException {
+        return null;
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employeee) throws EmployeeException {
+        return null;
+    }
+
+    @Override
+    public Employee deleteEmployeeById(Integer empId) throws EmployeeException {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getAllEmployee() throws EmployeeException {
+        return List.of();
+    }
 }
